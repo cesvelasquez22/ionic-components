73734bd0e837a01aab0b328a0038482189f8d8a9
Cesar Velasquez <cesar.velasquez@cit.hn>
2021-10-17 21:13:47 -0600

    + Final sección 4 + ion-list

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ComponentInterface} from '../../models/component.interface';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
+  components: ComponentInterface[] = [
+    {
+      icon: 'american-football-outline',
+      name: 'Action Sheet',
+      redirectTo: '/action-sheet'
+    },
+    {
+      icon: 'logo-apple-appstore',
+      name: 'Alert',
+      redirectTo: '/alert'
+    },
+  ];
 
   constructor() { }
 
