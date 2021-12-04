83a6c7b720e83ca80f13df97857ab82e2bef6771
Cesar Velasquez <cesar.velasquez@cit.hn>
2021-12-04 10:03:02 -0600

    + ion-item-sliding

--- src/app/pages/list/list.page.ts
+++ src/app/pages/list/list.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonList } from '@ionic/angular';
 import { Observable } from 'rxjs';
 import { DataService } from 'src/app/services/data.service';
 import { User } from 'src/app/services/data.type';
@@ -12,10 +13,27 @@ export class ListPage implements OnInit {
 
   users$: Observable<User[]>;
 
+  @ViewChild(IonList) ionList: IonList;
+
   constructor(private dataService: DataService) { }
 
   ngOnInit() {
     this.users$ = this.dataService.getUsers();
+  }
+
+  favorite(user: User) {
+    console.log('favorite', user);
+    this.ionList.closeSlidingItems();
+  }
+
+  share(user: User) {
+    console.log('share', user);
+    this.ionList.closeSlidingItems();
+  }
+
+  trash(user: User) {
+    console.log('trash', user);
+    this.ionList.closeSlidingItems();
   }
 
 }
